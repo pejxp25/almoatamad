69855ee9c107974dce42f667944c05c517ca945c
MOHAMMED ALHCHAIMI <s21410283@hmtech.alayen.edu.iq>
2026-09-08 23:26:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,4 @@
-const CACHE_NAME = 'almoatamad-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.svg'
-];
-
-// install
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting())
-  );
-});
-
-// activate (clean old caches)
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((k) => {
-          if (k !== CACHE_NAME) {
-            return caches.delete(k);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// fetch
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
-});
+const CACHE="almoatamad-v1"; const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./sw.js"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match("./index.html")))));
